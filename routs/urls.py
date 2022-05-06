533beed95f73725363ecd6c8b774db4e2f49b358
Petrov Mikhail <petrov.ma@icloud.com>
2022-05-06 16:03:55 +0300

    update url to re_path

--- routs/urls.py
+++ routs/urls.py
@@ -1,11 +1,11 @@
 from . import views
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^rout-list/$', views.RoutListView.as_view(), name='rout-list'),
-    url(r'^rout-detail/(?P<pk>\d+)/$', views.RoutDetailView.as_view(), name='rout-detail'),
-    url(r'^update-rout-list/$', views.UpdateRoutListView.as_view(), name='update-rout-list'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^rout-list/$', views.RoutListView.as_view(), name='rout-list'),
+    re_path(r'^rout-detail/(?P<pk>\d+)/$', views.RoutDetailView.as_view(), name='rout-detail'),
+    re_path(r'^update-rout-list/$', views.UpdateRoutListView.as_view(), name='update-rout-list'),
     # url(r'^update-rout-list/(?P<min_distance>\d+)(?P<max_distance>\d+)?$', views.UpdateRoutListView.as_view(), name='update-rout-list')
 ]
