5bbf6a425397f498bfc835567350345c8fe120c3
Petrov Mikhail <petrov.ma@icloud.com>
2022-05-15 22:02:42 +0300

    add route collections model

--- routs/admin.py
+++ routs/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Rout, Difficulty, Surface, Direction, Tag
+from .models import Rout, Difficulty, Surface, Direction, Tag, RouteCollections
 
 admin.site.register(Rout)
 admin.site.register(Difficulty)
 admin.site.register(Surface)
 admin.site.register(Direction)
 admin.site.register(Tag)
+admin.site.register(RouteCollections)
